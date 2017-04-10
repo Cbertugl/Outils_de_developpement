495c527a38a98c7cb2b6e8bff3c399e9fb2e636e
Camille B <camille.bertuglia@insa-cvl.fr>
2017-04-10 17:58:24 +0200

    Not sure if working - CSV

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -9,6 +9,18 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+        //int monmax = 0;
+        int nb = 0;
+        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+        List<String[]>myEntries = reader.readAll();
+        
+        //parcourt
+		for(int i=0; i<myEntries.size(); i++)
+            nb=myEntries.get(i);
+            System.out.println(nb);
+
+
+        System.out.println("Nombre lu:"+nb);
     }
 }
         
